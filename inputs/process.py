45e5968f7aed228827fba041b992210e412a863c
darkhaiku <pooya.gh.alamdari@hotmail.com>
2022-10-30 21:36:07 +0330

    learn about Class

--- inputs/process.py
+++ inputs/process.py
@@ -1,14 +1,28 @@
 import glob
 import os
 import shutil
+from PIL import Image
 
 inputs = glob.glob('inputs\*.jpg')
 
 class Digitizer:
     def __init__(self, filepath) -> None:
-        print(filepath)
+        self.filepath = filepath
+        self.img = Image.open(filepath)
+
+    def make_upside_down(self):
+        print("make upside down!")
+        self.img = self.img.rotate(180)
+
+    def save(self, output_filepath):
+        print('This has saved!!!')
+        self.img.save(output_filepath)
 
 os.makedirs('outputs_file', exist_ok=True)
 
 for filepath in inputs:
-    Digitizer(filepath)
+    output = filepath.replace('inputs', 'outputs_file')
+
+    image = Digitizer(filepath)
+    image.make_upside_down()
+    image.save(output)
